e8fbe139cd38d8a7b71830a45384b95bbbb766cb
SuhAilManzoor <Sohailmanzoorsario@gmail.com>
2025-01-10 18:35:31 +0500

    Item Remeber Application with the help of Listview Builder To Peroform CRUD Operations Add Edit Update Delete and Store

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:fifth_expanded_and_updating_list/03.list_view_builder_with_expandeds_scrollable.dart';
+import 'package:fifth_expanded_and_updating_list/04.Important_listview_updating_list.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ListviewBuilderWithExpandedsScrollable(),
+      home: ListviewUpdatingList(),
     );
   }
 }
